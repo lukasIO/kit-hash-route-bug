703c466b5181a065d63f334805c4a02d335105f1
lukasio <mail@lukasseiler.de>
2021-04-01 17:13:56 +0200

    init

---
+++ src/routes/blog/[slug]/detail/[detail].svelte
@@ -0,0 +1,6 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>{$page.params.detail}</h1>
+<a href="javascript:history.back()">go back</a>
---
+++ src/routes/blog/[slug]/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>{$page.params.slug}</h1>
+<a href="#content">anchor link to content</a>
+<section id="content">
+  Et omnis maxime quo ex. Ex id repellendus animi. Aut iste est et odio
+  necessitatibus eos ut. Consequatur culpa pariatur qui voluptates nam
+  accusantium. Qui ut eligendi dignissimos. Incidunt molestiae asperiores
+  officia adipisci. Reiciendis sit excepturi deleniti aliquam dolorem
+  consequatur omnis consequatur. Provident nisi explicabo eum nesciunt. Enim in
+  quaerat necessitatibus sit aperiam dolore et. Voluptatibus optio dolorem
+  beatae commodi fuga officia animi quidem. Atque quis autem est ut iure maxime
+  delectus. Autem ratione perspiciatis necessitatibus necessitatibus esse error
+  eius voluptas. Excepturi perspiciatis rerum nostrum et debitis. Illo ea sequi
+  et neque. Non quae voluptatem id ipsa adipisci tempora nemo. Excepturi et unde
+  nesciunt architecto est vero sed. Illo aut consequatur officiis sunt vel
+  doloribus fugit. Quaerat laboriosam eligendi eum voluptatum. Quasi modi quas
+  quia ut sit deleniti iure dignissimos. Provident iste sunt repudiandae velit
+  animi. Cumque illo consequuntur deleniti itaque et. Nam et libero repellat
+  nulla voluptas omnis omnis dicta. Et reiciendis nemo repellendus doloribus
+  laboriosam non. Aut error atque numquam recusandae voluptas.
+</section>
+<a href="/blog/my-blog-post/detail/my-detail">view detail</a>
